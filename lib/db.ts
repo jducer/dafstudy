21e864b1f2a26a2fec4ca71ce5dcee977fdcdb4b
jducer <jerryrobsonjr@gmail.com>
2026-04-19 13:49:11 -0400

    fix: resolve prisma 7 client initialziation by implementing native adapter-pg

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,19 @@
 // lib/db.ts
 import { PrismaClient } from './generated/main'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as { 
   prismaMain: PrismaClient 
 }
 
-export const prismaMain = globalForPrisma.prismaMain ?? new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-})
+const connectionString = process.env.DATABASE_URL
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
+export const prismaMain = globalForPrisma.prismaMain ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prismaMain = prismaMain
-}
   globalForPrisma.prismaMain = prismaMain
 }
 
